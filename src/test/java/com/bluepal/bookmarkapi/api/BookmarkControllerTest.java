11ccb09814bbcc1d032244c6a3cb51376c5b5c5f
petaharitha <peta.haritha26@gmail.com>
2024-03-01 17:21:51 +0530

    eigth  commit

--- src/test/java/com/bluepal/bookmarkapi/api/BookmarkControllerTest.java
+++ src/test/java/com/bluepal/bookmarkapi/api/BookmarkControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+//@TestPropertySource(properties = {
+//        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///demo"
+//})
 public class BookmarkControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -56,7 +60,13 @@ public class BookmarkControllerTest {
     void shouldGetBookmarks() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/bookmarks"))
                 .andExpect(status().isOk())
-                .andExpect((jsonPath("$.totalElements",CoreMatchers.equalTo(15))))
+                .andExpect(jsonPath("$.totalElements",CoreMatchers.equalTo(15)))
+                .andExpect(jsonPath("$.totalPages", CoreMatchers.equalTo(2)))
+                .andExpect(jsonPath("$.currentPage", CoreMatchers.equalTo(1)))
+                .andExpect(jsonPath("$.isFirst", CoreMatchers.equalTo(true)))
+                .andExpect(jsonPath("$.isLast", CoreMatchers.equalTo(false)))
+                .andExpect(jsonPath("$.hasNext", CoreMatchers.equalTo(true)))
+                .andExpect(jsonPath("$.hasPrevious", CoreMatchers.equalTo(false)))
 
 
         ;
